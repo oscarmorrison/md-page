a652eab219a0d1488a971146b1fe312b3ce1d355
Oscar Morrison <github@oscarmorrison.com>
2018-10-05 23:57:27 +1000

    update script add dev readme

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.body.style.padding = 20
     document.body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif'
-    var markdown = document.body.innerHTML
+    //handle github wrapping in pre
+    var markdown = document.body.childElementCount == 1 ? document.body.lastElementChild.innerHTML : document.body.innerHTML;
     var converter = new showdown.Converter()
     var html = converter.makeHtml(markdown);
     document.body.innerHTML = html
