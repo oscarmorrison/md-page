9be17429004ea7fb6896f80adb556f9ee571eafb
Oscar Morrison <github@oscarmorrison.com>
2018-10-05 23:48:02 +1000

    testing

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('hello world')
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.padding = 20
+    document.body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif'
+    var markdown = document.body.innerHTML
+    var converter = new showdown.Converter()
+    var html = converter.makeHtml(markdown);
+    console.log(html)
+    document.body.innerHTML = html
+    setTimeout(function() {
+        var hash = window.location.hash
+        window.location.hash = ''
+        window.location.hash = hash
+    }, 100)
+})
