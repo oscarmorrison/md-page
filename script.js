3f7555c5217f5358b2d09bbeb0c0291ba0483cf9
Oscar Morrison <github@oscarmorrison.com>
2018-10-05 23:54:21 +1000

    check for weird github

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-console.log('hello world')
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('ready')
     document.body.style.padding = 20
     document.body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif'
-    var markdown = document.body.innerHTML
+    var markdown = document.body.childElementCount == 1 ? document.body.lastElementChild.innerHTML : document.body.innerHTML;
     var converter = new showdown.Converter()
     var html = converter.makeHtml(markdown);
-    console.log(html)
     document.body.innerHTML = html
     setTimeout(function() {
         var hash = window.location.hash
