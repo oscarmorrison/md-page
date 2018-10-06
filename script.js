b946425d542fad45551c72e030d3a53b3a6fe225
Oscar Morrison <github@oscarmorrison.com>
2018-10-06 20:01:33 +1000

    use page title, or first words on page as title

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.style.padding = 20
     document.body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif'
     //handle github wrapping in pre
-    var markdown = document.body.childElementCount == 1 ? document.body.lastElementChild.innerHTML : document.body.innerHTML;
+    var markdown = document.body.childElementCount == 1 ? document.body.firstElementChild.innerHTML : document.body.innerHTML;
     var converter = new showdown.Converter()
     var html = converter.makeHtml(markdown);
     document.body.innerHTML = html
+    document.title = document.title || document.body.firstElementChild.innerText.trim()
     setTimeout(function() {
         var hash = window.location.hash
         window.location.hash = ''
