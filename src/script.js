2d2f4452cc84bb0fe44b50605705e19b045e683f
hopokuan <hopokuan@umich.edu>
2020-05-12 23:55:12 -0700

    Added polyfill for string includes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+// Polyfill from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes 
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+    
+        if (search instanceof RegExp) {
+            throw TypeError('first argument must not be a RegExp');
+        } 
+        if (start === undefined) { start = 0; }
+        return this.indexOf(search, start) !== -1;
+    };
+}
+
 // Polyfill from https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/prepend#Polyfill
 (function (arr) {
     arr.forEach(function (item) {
